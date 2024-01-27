158ad94dcfe9c6064c340fa8191023661c1ab574
Milman <milindmjoshi@live.com>
2024-01-27 13:07:55 -0500

    add Clear Day button, clean up code, update ReadME

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,63 +2,60 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+
   var saveButtons = $('.saveBtn');
-      console.log(saveButtons);
-      saveButtons.each(function(element){
-          saveButtons[element].addEventListener('click',()=>  {
-            var parentDivId = this.parentElement.getAttribute('id');
-            var timeTextArea = this.parentElement.children[1];
-            console.log(timeTextArea.value);
-            localStorage.setItem(parentDivId,timeTextArea.value);
-        })
-      });
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  var clearButton = $('.clearBtn');
+  var timeblocks = $('.time-block');
+  var currentDate = $('#currentDay');
+  var currentHour = dayjs().format('H');
+  console.log(currentHour);
+  // test different Hours
+  //currentHour = 13;
 
-      var currentHour = dayjs().format('H');
-      console.log(currentHour);
-      // test different Hours
-      currentHour = 13;
 
-      // get all elements of class 'row time block'
-      var timeblocks = $('.time-block');
-      console.log(timeblocks);
-      timeblocks.each(function(element){
-          //console.log(timeblocks[element]);
-          var blockId = (timeblocks[element].getAttribute('id'));
-          var blockIdHour = parseInt(blockId.substring(5));
-          console.log(element);
-          console.log(blockIdHour);
-          if (blockIdHour == currentHour){
-            timeblocks[element].className += 'present';
-          }
-          else if (blockIdHour > currentHour){
-            console.log('set future');
-            timeblocks[element].className += 'future';
-            console.log(timeblocks[element].className);
-          }
-          else{
-            timeblocks[element].className += 'past';
-            console.log(timeblocks[element].className);
-          }
-      })
-       
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  timeblocks.each(function(element){
+
+  // Add a listener for click events on the clear button.
+  // This button is used to clear all entries from the calendar 
+  clearButton[0].addEventListener('click',()=> clearDay());
+
+  // Add a listener for click events on the save buttons array.
+  // Use the id in the parent div of the time-block as a key to save the user input in
+  // local storage. 
+  saveButtons.each(function (element) {
+    saveButtons[element].addEventListener('click', () => {
+      var parentDivId = this.parentElement.getAttribute('id');
+      var timeTextArea = this.parentElement.children[1];
+      console.log(timeTextArea.value);
+      localStorage.setItem(parentDivId, timeTextArea.value);
+    })
+  });
+
+  // get all elements of class 'row time block' and apply the past, present, or 
+  // future class to each time block by comparing the id to the current hour
+  
+  timeblocks.each(function (element) {
+    //console.log(timeblocks[element]);
+    var blockId = (timeblocks[element].getAttribute('id'));
+    var blockIdHour = parseInt(blockId.substring(5));
+    console.log(element);
+    console.log(blockIdHour);
+    if (blockIdHour == currentHour) {
+      timeblocks[element].className += 'present';
+    }
+    else if (blockIdHour > currentHour) {
+      console.log('set future');
+      timeblocks[element].className += 'future';
+      console.log(timeblocks[element].className);
+    }
+    else {
+      timeblocks[element].className += 'past';
+      console.log(timeblocks[element].className);
+    }
+  })
+
+  // Get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements using the id
+  timeblocks.each(function (element) {
     //console.log(timeblocks[element]);
     var divElement = timeblocks[element];
     var blockId = (divElement.getAttribute('id'));
@@ -66,10 +63,29 @@ $(function () {
     var itemText = localStorage.getItem(blockId);
     var timeTextArea = divElement.children[1];
     console.log(timeTextArea);
+    //set max lenght of 200 char to limit the amount data user can enter for each time
+    timeTextArea.setAttribute("maxlength","200");
     timeTextArea.value = itemText;
   })
-    
-  // TODO: Add code to display the current date in the header of the page.
-  var currentDate = $('#currentDay');
-   currentDate.text(dayjs().format('dddd, MMMM DD'));
+
+  //Display the current date in the header of the page.
+  currentDate.text(dayjs().format('dddd, MMMM DD'));
+
+  // Clear all data  out of local storage for the day for each time block
+  // This function is called when the clear button is clicked
+  function clearDay() {
+    console.log("Clear Day");
+    var timeblocks = $('.time-block');
+    timeblocks.each(function (element) {
+      //console.log(timeblocks[element]);
+      var divElement = timeblocks[element];
+      var blockId = (divElement.getAttribute('id'));
+      console.log(blockId);
+      localStorage.setItem(blockId,"");
+      var timeTextArea = divElement.children[1];
+      console.log(timeTextArea);
+      timeTextArea.value = "";
+    })
+
+  }
 });
